bcf682cec784f8c935ee5da9fff457902f9e0647
patrick.wabnitz <patrick.wabnitz@ext.avenga.com>
2023-10-23 13:30:54 +0200

    - setup login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { MinimalHeaderComponent } from './components/minimal-header/minimal-header.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
