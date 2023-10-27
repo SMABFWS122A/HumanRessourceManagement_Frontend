34a375c81ad6b94fd818861f507dd32c205408ff
sebastiant <sebastian.thoenes@gmx.de>
2023-10-27 11:45:48 +0200

    - add buttonClickHandler in time-booking.component for alert output

--- frontend/src/app/components/time-booking/time-booking.component.ts
+++ frontend/src/app/components/time-booking/time-booking.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./time-booking.component.css']
 })
 export class TimeBookingComponent {
+  buttonClickHandler(buttonID: string){
+    let time = new Date().toLocaleTimeString();
+    if (buttonID === "comming") {
+      alert("Kommen wurde gestempelt um " + time);
+    } else {
+      alert("Gehen wurde gestempelt um " + time);
+    }
+  }
 
 }
