0b75214a75159e11584b15ee96a85137fdfb70a0
sebastiant <sebastian.thoenes@gmx.de>
2023-11-02 10:23:44 +0100

    - setup data connection login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Login} from "../../model/login";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit{
+
+  loginResult!: Login;
+  constructor(private client: HttpClient) {
+  }
+  ngOnInit() {
+  this.client.get<Login>('http://localhost:8080/login/hansmueller@mail.de')
+    .subscribe(data => {
+      if (data.email === "hansmueller@mail.de") {
+        this.loginResult = data;
+        console.log(this.loginResult.passwort)
+      } else {
+        console.error('Unerwartete Datenstruktur oder Eigenschaft "email" nicht gefunden.');
+      }
+    });
+  }
 }
