939ad4e4353d327bada0a9d83a7e1760bc30ec64
patrick.wabnitz <patrick.wabnitz@ext.avenga.com>
2023-11-02 14:02:16 +0100

    added digital clock to clock component

--- frontend/src/app/components/clock/clock.component.ts
+++ frontend/src/app/components/clock/clock.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
   styleUrls: ['./clock.component.css']
 })
-export class ClockComponent {
+export class ClockComponent implements OnInit{
 
+  currentTime!: Date;
+
+   constructor() { }
+
+   ngOnInit() {
+     this.updateClock();
+     setInterval(() => this.updateClock(), 1000);
+   }
+
+   updateClock() {
+     this.currentTime = new Date();
+   }
 }
