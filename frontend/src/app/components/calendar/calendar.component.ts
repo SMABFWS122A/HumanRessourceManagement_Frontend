6a21b570af180742dbde4e0ab63d7efe195d0b96
sebastiant <sebastian.thoenes@gmx.de>
2023-11-05 23:54:51 +0100

    - modified calendar.component days li tags

--- frontend/src/app/components/calendar/calendar.component.ts
+++ frontend/src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
@@ -7,7 +7,7 @@ import { Component, AfterViewInit } from '@angular/core';
 })
 
 
-export class CalendarComponent implements AfterViewInit {
+export class CalendarComponent implements OnInit {
 
   daysTag!: HTMLElement;
   currentDate!: HTMLElement;
@@ -17,11 +17,14 @@ export class CalendarComponent implements AfterViewInit {
   currMonth!: number;
   months!: string[];
 
+  classes:string[] = [];
+  days: number[] = [];
+
   constructor() {
 
   }
 
-  ngAfterViewInit() {
+  ngOnInit() {
     this.daysTag = document.querySelector(".days") as HTMLElement;
     this.currentDate = document.querySelector(".current-date") as HTMLElement;
     this.prevNextIcon = document.querySelectorAll(".icons span");
@@ -44,6 +47,10 @@ export class CalendarComponent implements AfterViewInit {
           default:
             this.currMonth = this.currMonth + 1;
         }
+
+        this.classes = [];
+        this.days = [];
+
         if (this.currMonth < 0 || this.currMonth > 11) {
           this.date = new Date(this.currYear, this.currMonth, this.date.getDate());
           this.currYear = this.date.getFullYear();
@@ -61,23 +68,24 @@ export class CalendarComponent implements AfterViewInit {
     const lastDateofMonth = new Date(this.currYear, this.currMonth + 1, 0).getDate();
     const lastDayofMonth = new Date(this.currYear, this.currMonth, lastDateofMonth).getDay() === 0 ? 7 : new Date(this.currYear, this.currMonth, lastDateofMonth).getDay();
     const lastDateofLastMonth = new Date(this.currYear, this.currMonth, 0).getDate();
-    let liTag = "";
+
 
     for (let i = firstDayofMonth; i > 1; i--) {
-      liTag += `<li _ngcontent-ng-c2770918467 class="inactive">${lastDateofLastMonth - i + 2}</li>`;
+      this.classes.push("inactive");
+      this.days.push(lastDateofLastMonth - i + 2);
     }
 
     for (let i = 1; i <= lastDateofMonth; i++) {
       const isToday = i === this.date.getDate() && this.currMonth === this.date.getMonth() && this.currYear === this.date.getFullYear() ? "active" : "";
-      liTag += `<li _ngcontent-ng-c2770918467 class="${isToday}">${i}</li>`;
+      this.classes.push(isToday);
+      this.days.push(i);
     }
 
     for (let i = lastDayofMonth; i < 7; i++) {
-      liTag += `<li _ngcontent-ng-c2770918467 class="inactive">${i - lastDayofMonth + 1}</li>`;
+      this.classes.push("inactive");
+      this.days.push(i - lastDayofMonth + 1);
     }
-
     this.currentDate.innerText = `${this.months[this.currMonth]} ${this.currYear}`;
-    this.daysTag.innerHTML = liTag;
   }
 }
 
